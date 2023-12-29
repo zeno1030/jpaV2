7871a01f7472fa759af3dd62b2de5e1d09c127ec
ZENO <zeno1030@naver.com>
2023-12-29 18:40:57 +0900

    ㅓãJPA에서 DTO 직접 조회 - 컬렉션 조회 최적화

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -6,9 +6,13 @@ import jpabook.jpashop.domain.OrderItem;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
@@ -19,6 +23,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class OrderApiController {
     private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
 
     @GetMapping("/api/v1/orders")
     public List<Order> ordersV1(){
@@ -41,10 +46,30 @@ public class OrderApiController {
                 .map(OrderResponseDto::new)
                 .collect(Collectors.toList());
     }
-    @GetMapping("/api/v3/orders")
+    @GetMapping("/api/v3/orders") //페이징 처리를 할 수가 없다.
     public List<OrderResponseDto> oderV3(){
-        List<Order> allWithItem = orderRepository.findAllWithItem();
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderResponseDto> result = orders.stream().map(o -> new OrderResponseDto(o)).collect(Collectors.toList());
+
+        return result;
     }
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderResponseDto> oderV3_page(@RequestParam(value = "offset", defaultValue = "0") int offset,
+                                              @RequestParam(value = "limit", defaultValue = "100") int limit)
+    {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        return orders.stream().map(OrderResponseDto::new).collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> orderV4(){
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
 
     @Data
     static class OrderResponseDto{
